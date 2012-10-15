c867717993be0dcbd37aa36806593a6fdf7324f7
Pere Martir <pere.martir4@gmail.com>
2012-10-15 14:22:04 +0200

    init the board randomly and draw all diamonds

--- MidasMiner/pngDrvr.cpp
+++ MidasMiner/pngDrvr.cpp
@@ -16,7 +16,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include <GLUT/glut.h>
+
+#include <vector>
+
 extern "C" {
 #include "pngLoad.h"
 }
@@ -29,15 +33,6 @@ static GLuint texName;
 int kWindowWidth = 320; /* window width */
 int kWindowHeight = 320; /* window height */
 
-
-class Board
-{
-};
-
-/* int setupGLTexture char* int int int
- * Function loads image from buffer into
- * OpenGL texture.
- */
 
 
 int setupGLTexture(char *image, int width, int height, GLuint texName) 
@@ -65,6 +60,109 @@ int setupGLTexture(char *image, int width, int height, GLuint texName)
     
     return 1; 
 }
+
+class Board
+{
+public:
+    Board();     
+    
+    void initWithDiagonalLines();
+    void initRandomly();
+    void draw();
+    
+private:
+    typedef unsigned int DiamondIndex;
+    typedef std::vector<std::vector<DiamondIndex> > DiamondArray;
+    
+
+    DiamondArray m_diamondArray;
+    static const unsigned int SIZE = 8;
+    //unsigned int m_diamonds;
+    std::vector<GLuint> m_diamondTextures;
+};
+
+Board::Board()
+{
+    //m_diamonds = 0;
+    const char* filenames[] = {"Blue.png", "Green.png", "Purple.png", "Red.png", "Yellow.png" };
+    for (unsigned int i = 0; i < sizeof(filenames) / sizeof(filenames[0]); ++ i) {
+        unsigned int width = 0, height = 0;
+        char* imageRawData = NULL;
+        int result = pngLoad(const_cast<char*>(filenames[i]), &width, &height, &imageRawData);
+        if (result == 0)
+        {
+            printf("(pngLoad) %s FAILED.\n", filenames[i]);
+            abort();
+        }
+        
+        GLuint textureName = 0;
+        glGenTextures(1, &textureName);
+        assert(textureName != 0);
+        setupGLTexture(imageRawData, width, height, textureName);
+        m_diamondTextures.push_back(textureName);
+    }
+    assert(!m_diamondTextures.empty());
+}
+
+void Board::initRandomly()
+{
+    srand(time(NULL));
+    for (unsigned int i = 0; i < SIZE; ++ i) {
+        std::vector<DiamondIndex> row;
+        //row.resize(SIZE, 0);
+        for (unsigned int j = 0; j < SIZE; ++ j)
+            row.push_back(rand() % m_diamondTextures.size());
+        m_diamondArray.push_back(row);
+    }
+}
+
+void Board::initWithDiagonalLines()
+{
+}
+    
+void Board::draw()
+{
+    glEnable (GL_TEXTURE_2D); 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    
+    const unsigned int DIAMOND_SIZE = 40;
+    for (unsigned int i = 0; i < SIZE; ++ i) {
+        for (unsigned int j = 0; j < SIZE; ++ j) {
+            
+            unsigned int left = i * DIAMOND_SIZE, top = j * DIAMOND_SIZE;
+            DiamondIndex d = m_diamondArray[i][j];
+            assert(d < m_diamondTextures.size());
+            glBindTexture (GL_TEXTURE_2D, m_diamondTextures[d]);
+            
+            glBegin (GL_QUADS);
+            
+            glTexCoord2f (0.0f, 0.0f); // upper left 
+            glVertex2f (left, top);
+            
+            glTexCoord2f (1.0f, 0.0f); // upper right
+            glVertex2f (left + DIAMOND_SIZE, top);
+            
+            glTexCoord2f (1.0f, 1.0f); // lower right
+            glVertex2f (left + DIAMOND_SIZE, top + DIAMOND_SIZE);
+            
+            glTexCoord2f (0.0f, 1.0f); // lower left
+            glVertex2f (left, top + DIAMOND_SIZE);
+            
+            glEnd ();
+        }
+    }
+}
+    
+
+//const unsigned int Board::SIZE = 8;
+
+/* int setupGLTexture char* int int int
+ * Function loads image from buffer into
+ * OpenGL texture.
+ */
+
+
+
 
 /*
  * void init void 
@@ -120,9 +218,7 @@ void display(void)
 
 
 
-  glEnable (GL_TEXTURE_2D); /* enable texture mapping */
-  glBindTexture (GL_TEXTURE_2D, texName); /* bind to our texture*/
-    
+
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 #if 0
@@ -131,6 +227,10 @@ void display(void)
     glMatrixMode (GL_MODELVIEW);
     glLoadIdentity ();
     glTranslatef (0, 0, -20); /* eye position */
+    
+    glEnable (GL_TEXTURE_2D); /* enable texture mapping */
+    glBindTexture (GL_TEXTURE_2D, texName); /* bind to our texture*/
+    
 
   /* setup texture map using vertices from OpenGL Texture Mapping,
    * an Introduction, by Nate Miller.This image buffer has a 256 x 256 
@@ -160,7 +260,33 @@ void display(void)
     // http://basic4gl.wikispaces.com/2D+Drawing+in+OpenGL
     glTranslatef (0.375, 0.375, 0);
 
+    static Board *board = NULL;
+    if (!board) {
+        board = new Board();
+        board->initRandomly();
+        //board->initWithDiagonalLines();
+        /*
+        std::vector<Board::DiamondIndex> row;
+        for (unsigned i = 0; i < Board::SIZE; ++ i)
+        row.push_back(0);
+        row.push_back(1);
+        row.push_back(2);
+        row.push_back(3);
+        row.push_back(4);
+        row.push_back(3);
+        row.push_back(2);
+        row.push_back(1);
+        
+        Board::DiamondArray array;
+        array.push_back(row);
+        */
+        
+
+    }
+    board->draw();
+#if 0
     glBegin (GL_QUADS);
+    
     glTexCoord2f (0.0f, 0.0f); /* lower left corner of image */
     glVertex2f (0, 32);
     
@@ -172,7 +298,10 @@ void display(void)
     
     glTexCoord2f (0.0f, 1.0f); /* upper left corner of image */
     glVertex2f (0, 0);
+    
     glEnd ();
+#endif    
+
     
 #endif
 
